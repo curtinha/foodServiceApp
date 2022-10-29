2964120f2c67dd5b2d9701ed2c8f215af59165a9
Curtis Osborne <curosbo@chbc.school.nz>
2022-10-30 08:30:16 +1300

    Created table and changed references to include SQL

---
+++ foodServiceApp/Resources/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[window]
+(
+	[foodID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [foodName] VARCHAR(50) NOT NULL, 
+    [foodType] NCHAR(10) NOT NULL, 
+    [dateAdded] DATE NOT NULL, 
+    [expiryDate] DATE NOT NULL
+)
---
+++ foodServiceApp/dbo.window.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[foodID] INT NOT NULL PRIMARY KEY, 
+    [foodName] VARCHAR(50) NOT NULL, 
+    [foodType] NCHAR(10) NOT NULL, 
+    [dateAdded] DATE NOT NULL, 
+    [expiryDate] DATE NOT NULL
+)
